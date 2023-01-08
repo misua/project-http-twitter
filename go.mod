f4f012fbcecaa8fd84319f491110fdaf58d5fcb5
Charles <charlespogzz@googlemail.com>
2023-01-08 06:35:02 +0100

    ugauga

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module project-http-twitter
 
 go 1.18
-
-require github.com/google/uuid v1.3.0
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 type userPayload struct {
@@ -15,7 +17,14 @@ type userPayload struct {
 	IDCounter int
 }
 
-func createTweet(w http.ResponseWriter, r *http.Request) (int, error) {
+// type response struct {
+
+// 	Id int `json:"ID"`
+
+// }
+
+//
+func createTweet(w http.ResponseWriter, r *http.Request) {
 	var u userPayload
 
 	defer r.Body.Close()
@@ -24,29 +33,36 @@ func createTweet(w http.ResponseWriter, r *http.Request) (int, error) {
 
 	if err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
-		return 0, err
+		return
+		// fmt.Println("Tweet:", u.Message)
+		// fmt.Println("Location:", u.Location)
+
+		fmt.Printf("Tweet: `%s` from %s\n", u.Message, u.Location)
+
+		w.WriteHeader(http.StatusOK)
+
+		// u.ID = u.NextID
+		// u.NextID++
+		// u.IDCounter++
+
+		u := &userPayload{ID: 1}
+
+		encoder := json.NewEncoder(os.Stdout)
+		err = encoder.Encode(u)
+		if err != nil {
+			log.Fatal("error encoding user: %v", err)
+		}
+
+		//return u.ID, nil
+
+		u.ID++
+		if err := encoder.Encode(u); err != nil {
+			log.Fatalf("error encoding user: %v", err)
+		}
+		fmt.Println(u.ID)
+
 	}
-	// fmt.Println("Tweet:", u.Message)
-	// fmt.Println("Location:", u.Location)
-
-	fmt.Printf("Tweet: `%s` from %s\n", u.Message, u.Location)
-
-	w.WriteHeader(http.StatusOK)
-
-	u.ID = u.NextID
-	u.NextID++
-	u.IDCounter++
-
-	encoder := json.NewEncoder(w)
-	err = encoder.Encode(u)
-	if err != nil {
-		return 0, err
-	}
-
-	return u.ID, nil
-
 }
-
 func main() {
 
 	http.HandleFunc("/tweets", createTweet)
